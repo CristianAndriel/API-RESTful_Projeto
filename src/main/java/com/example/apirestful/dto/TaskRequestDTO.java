ec3bdea8fbbe6c859bebe581af657db47f3478de
CristianAndriel <cristianandrielsilva@gmail.com>
2026-02-12 18:58:22 -0300

    create: DTOs criado

--- src/main/java/com/example/apirestful/dto/TaskRequestDTO.java
+++ src/main/java/com/example/apirestful/dto/TaskRequestDTO.java
@@ -1,6 +1,16 @@
 package com.example.apirestful.dto;
 
-public class TaskRequestDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
+public class TaskRequestDTO {
+    @NotBlank(message = "Título é obrigatório")
+    @Size(min = 3, max = 200, message = "O título deve ter entre 3 e 200 caracteres")
+    private String title;
+
+    @Size(max = 1000, message = "A descrição deve ter no máximo 1000 caracteres")
+    private String description;
 }
 
