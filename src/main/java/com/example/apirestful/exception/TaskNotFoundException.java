ac7073fbf06072500952ec6271845d86e8983923
CristianAndriel <cristianandrielsilva@gmail.com>
2026-02-12 19:19:39 -0300

    exceptions

--- src/main/java/com/example/apirestful/exception/TaskNotFoundException.java
+++ src/main/java/com/example/apirestful/exception/TaskNotFoundException.java
@@ -1,6 +1,8 @@
 package com.example.apirestful.exception;
 
 public class TaskNotFoundException extends RuntimeException {
-
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
 }
 
