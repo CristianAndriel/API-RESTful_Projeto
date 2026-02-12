6b82ad65c6218e27d1deee8a014b9fa88f77d763
CristianAndriel <cristianandrielsilva@gmail.com>
2026-02-12 19:04:49 -0300

    fix: repository

--- src/main/java/com/example/apirestful/repository/TaskRepository.java
+++ src/main/java/com/example/apirestful/repository/TaskRepository.java
@@ -1,16 +1,14 @@
 package com.example.apirestful.repository;
 
-import com.seunome.taskapi.model.Task;
+import com.example.apirestful.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TaskRepository {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findByCompleted(Boolean completed);
-
     List<Task> findByTitleContainingIgnoreCase(String title);
 }
-
