80ef9de97968fc968b7b18d6c44584c019783c1e
CristianAndriel <cristianandrielsilva@gmail.com>
2026-02-12 19:01:59 -0300

    create: repository criado

--- src/main/java/com/example/apirestful/repository/TaskRepository.java
+++ src/main/java/com/example/apirestful/repository/TaskRepository.java
@@ -1,6 +1,16 @@
 package com.example.apirestful.repository;
 
+import com.seunome.taskapi.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface TaskRepository {
 
+    List<Task> findByCompleted(Boolean completed);
+
+    List<Task> findByTitleContainingIgnoreCase(String title);
 }
 
