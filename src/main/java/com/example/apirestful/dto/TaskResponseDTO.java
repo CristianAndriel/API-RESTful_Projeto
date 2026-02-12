1e0aa611451bbc82d357a27c696f9b1f9761e31c
CristianAndriel <cristianandrielsilva@gmail.com>
2026-02-12 19:00:27 -0300

    create: dto response

--- src/main/java/com/example/apirestful/dto/TaskResponseDTO.java
+++ src/main/java/com/example/apirestful/dto/TaskResponseDTO.java
@@ -1,6 +1,20 @@
 package com.example.apirestful.dto;
 
-public class TaskResponseDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskResponseDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean completed;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
 
